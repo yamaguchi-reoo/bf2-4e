4460289e2fd989c931ffbceea009a1baca1acfc7
Nakora-koutyan <104492122+Nakora-koutyan@users.noreply.github.com>
2023-07-04 15:02:28 +0900

    Playerクラスの作成

--- bf2-4e/Player.h
+++ bf2-4e/Player.h
@@ -1,1 +1,13 @@
 #pragma once
+
+class Player
+{
+public:
+	int PlayerImages[30];
+
+public:
+	Player();			//コンストラクタ
+	~Player();			//デストラクタ
+	void Update();		//描画以外に関することを実装する
+	void Draw()const;	//描画に関することを実装する
+};
