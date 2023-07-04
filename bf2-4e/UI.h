32b262f8657601fde5499cf06e0958e4be2bf18e
モトラ２０２２ <sorato-motomura-k22f@stu.kbc.ac.jp>
2023-07-04 15:03:02 +0900

    変数の記入
    お腹痛くてなにも出来なかった（言い訳です）

---
+++ bf2-4e/UI.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class UI
+{
+private:
+	int Score;	//スコア
+
+
+public:
+	//コンストラクタ
+	UI();
+	//デストラクタ
+	~UI();
+
+	// 描画以外の更新を実装
+	void Update();
+
+	// 描画に関することを実装
+	void  Draw() const;
+};
+
+
+
+
