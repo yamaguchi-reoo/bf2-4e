e25d9b918e2b306569f34be6ccb7a9279a1a5d4f
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-07-04 15:02:30 +0900

    緑色の敵画像読込

--- bf2-4e/Enemy.cpp
+++ bf2-4e/Enemy.cpp
@@ -4,6 +4,25 @@
 // コンストラクタ
 Enemy::Enemy()
 {
+    // 初期化処理
+    try
+    {
+        //画像読込
+        LoadDivGraph("source/Resource/images/Enemy/Enemy_G_Animation.png", 19, 8, 3, 32, 32, enemy_pink_image);
+
+        ////LogoImage = LoadGraph("source/Resource/images/Title/Title_Logo.png");
+        //if (enemy_pink_image == -1)
+        //{
+        //    throw "source/Resource/images/Title/Title_Logo.png";
+        //}
+    }
+    catch (int& err)
+    {
+        printf("エラーコード%d\n", err);
+    }
+
+    if (LoadDivGraph("images/block.bmp", 19, 8, 3, 32, 32, enemy_pink_image) == -1);
+
 
 }
 
