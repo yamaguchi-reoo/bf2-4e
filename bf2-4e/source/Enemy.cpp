b61b067ca00bcb7b151fce8262a98a023665a7aa
chihiro-hiro <120348424+chihiro-hiro@users.noreply.github.com>
2023-07-10 23:03:22 +0900

    スタート時の敵の浮上処理、敵が風船を膨らませるアニメーション処理の追加

--- bf2-4e/source/Enemy.cpp
+++ bf2-4e/source/Enemy.cpp
@@ -15,8 +15,20 @@ Enemy::Enemy()
 	LoadDivGraph("Source/Resource/images/Enemy/Enemy_G_Animation.png", 19, 8, 3, 64, 64, enemy_green_image);
 	LoadDivGraph("Source/Resource/images/Enemy/Enemy_R_Animation.png", 19, 8, 3, 64, 64, enemy_red_image);
 
+	enemy_x = 32;
+	enemy_y = 0;
+	enemy_speed = 0;
+
 	fps_count = 0;
 	animation_pattern_number = 0;
+	second = 0;
+
+	inflat_bealloon_count = 0;
+
+	now_image = 0;
+	next_image = 0;
+
+	enemy_state = EnemyState::kInflatBealloon;
 }
 
 // デストラクタ
@@ -30,35 +42,92 @@ void Enemy::Update()
 {
 	fps_count++;
 
+
 	if (fps_count >= 60)
 	{
+		// 秒数のカウントを増やす
+		second++;
+
 		// fpsのカウントを0に戻す
 		fps_count = 0;
 	}
 
-	// 風船を膨らませるアニメーション処理
-	InflatBealloon();
+	switch (enemy_state)
+	{
+		case EnemyState::kInflatBealloon:
+			// 風船を膨らませるアニメーション処理
+			InflatBealloon();
+			break;
+		case EnemyState::kFlight:
+			break;
+		case EnemyState::kParachute:
+			break;
+		case EnemyState::kUpright:
+			break;
+		case EnemyState::kDeath:
+			break;
+		default:
+			break;
+	}
+
 }
 
 // 描画に関することを実装
 void Enemy::Draw() const
 {
 	// ステージ１に敵の画像の描画
-	DrawRotaGraph(200, 252, 1, 0, enemy_pink_image[animation_pattern_number], TRUE, FALSE);
-	DrawRotaGraph(300, 252, 1, 0, enemy_pink_image[animation_pattern_number], TRUE, FALSE);
-	DrawRotaGraph(400, 252, 1, 0, enemy_pink_image[animation_pattern_number], TRUE, FALSE);
+	//DrawRotaGraph(200, 252, 1, 0, enemy_pink_image[animation_pattern_number], TRUE, FALSE);
+
+	switch (enemy_state)
+	{
+	case Enemy::EnemyState::kInflatBealloon:
+		// 風船を膨らまし切ると浮き上がる
+		DrawRotaGraph(200, 252 - enemy_y, 1, 0, enemy_pink_image[animation_pattern_number], TRUE, FALSE);
+		break;
+	case Enemy::EnemyState::kFlight:
+		break;
+	case Enemy::EnemyState::kParachute:
+		break;
+	case Enemy::EnemyState::kUpright:
+		break;
+	case Enemy::EnemyState::kDeath:
+		break;
+	default:
+		break;
+	}
+
+	DrawFormatString(10, 10, 0xFFFFFF, "秒数%5d", second);
 
 }
 
 // 風船を膨らませるアニメーション処理
 void Enemy::InflatBealloon()
 {
-	if (fps_count <= 30)
+	inflat_bealloon_count++;
+
+	next_image = inflat_bealloon_count / 22;
+
+	if (now_image != next_image)
 	{
-		animation_pattern_number = 1;
+		now_image = next_image;
+		animation_pattern_number = next_image;
 	}
-	else
+
+	if (inflat_bealloon_count >= 180 && enemy_y <= 20)
 	{
-		animation_pattern_number = 0;
+		// 敵を浮上させる
+		enemy_y++;
+
+		// 浮上時の画像へ切り替え
+		animation_pattern_number = 8;
 	}
+	else if(inflat_bealloon_count >= 180)
+	{
+		// カウントを0に戻す
+		inflat_bealloon_count = 0;
+
+		// 敵の状態遷移
+		enemy_state = EnemyState::kFlight;
+	}
+
 }
--- bf2-4e/source/Enemy.h
+++ bf2-4e/source/Enemy.h
@@ -8,8 +8,29 @@ private:
 	int enemy_green_image[19];				// 緑色の敵の画像
 	int enemy_red_image[19];				// 赤色の敵の画像
 
+	int enemy_x;							// 敵のX座標
+	int enemy_y;							// 敵のY座標
+	int enemy_speed;						// 敵のスピード
+
 	int fps_count;							// fpsのカウント
 	int animation_pattern_number;			// アニメーションパターンの番号
+	int second;								// 秒数のカウント
+
+	int inflat_bealloon_count;				// 風船を膨らましきるまでのカウント
+
+	int now_image;							// 今から表示される画像
+	int next_image;							// 次に表示される画像
+
+	enum class EnemyState
+	{
+		kInflatBealloon,					// 風船を膨らませる
+		kFlight,							// 空中で羽ばたく
+		kParachute,							// パラシュート
+		kUpright,							// 直立
+		kDeath								// 死亡
+	};
+
+	EnemyState enemy_state;						// 敵の状態
 
 public:
 	// コンストラクタ
