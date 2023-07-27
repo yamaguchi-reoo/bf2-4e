f4256f1d3144395b87994028db051dcb5bafc666
モトラ２０２２ <sorato-motomura-k22f@stu.kbc.ac.jp>
2023-07-27 14:59:11 +0900

    雷雲、稲光の作成

--- bf2-4e/source/Thunder.h
+++ bf2-4e/source/Thunder.h
@@ -10,26 +10,30 @@ private:
 	int thunder_image[6];
 	int thunder_effect_image[3];
 
-	/*アニメーションの待機変数*/
+	/*アニメーションの待機用変数*/
 	//雷雲
-	int cloud_time;
 	int cloud_anime_num;
 
-	void GetNowDrawScreenUpdateTime();
-	const int thirty_seconds;
+	int thunder_cloud_frame = 60;	//制限時間
+	int thunder_cloud_anime_frame;	//雷雲のアニメーション用のフレーム
 
-	int thunder_cloud_time;
-	int thunder_cloud_anime_num;
+	//雷雲のアニメーション
+	int thunder_cloud_time;			
+	int thunder_cloud_anime_num;	
 
-	//かみなり
-	int thunder_time;
-	int thunder_anime_num;
+	//稲光
+	int thunder_time_anime;		//
+	int thunder_anime_num;		//
+
+	int thunder_frame = 60;		//制限時間
+	int thunder_anime_frame;	//稲光のアニメーション用のフレーム
+
+	bool is_thunder_shoot_ready;	//稲光を出すかどうか
+	bool thunder_shoot_flg;			//稲光が出ているどうか
 
 	//雷モドキ
 	int thunder_effect_time;
 	int thunder_effect_anime_num;
-
-	Fps fps();
 
 public:
 	//コンストラクタ
