800ffa2a5b5ddb1c9e9e24b119f75cba0bf3b926
shimoji0713 <120348413+shimoji0713@users.noreply.github.com>
2023-07-28 10:45:43 +0900

    シャボン玉の挙動追加

--- bf2-4e/source/Bubble.cpp
+++ bf2-4e/source/Bubble.cpp
@@ -1,5 +1,6 @@
 #include "DxLib.h"
 #include "Bubble.h"
+#include <math.h>
 
 Bubble::Bubble()
 {
@@ -16,6 +17,19 @@ Bubble::Bubble()
 
 	location.x = 50;
 	location.y = 70;
+	erea.height = 64;
+	erea.width = 64;
+	erea.width_rate = 0;
+	erea.height_rate = 0;
+
+
+	PosX = 0;		// 描画座標X
+	PosY = 0;		// 描画座標Y
+	Radius = 0;		// 半径(描画用)
+	CenterX = 100;	// 中心座標X
+	CenterY = 400;	// 中心座標Y
+	Angle = 0;		// 角度
+	Length = 30;		// 半径の長さ
 }
 
 Bubble::~Bubble()
@@ -26,6 +40,7 @@ Bubble::~Bubble()
 void Bubble::Update()
 {
 	BubbleAnimation();
+	MoveBubble();
 }
 
 void Bubble::Draw() const
@@ -50,4 +65,26 @@ void Bubble::BubbleAnimation(void)
 		// カウントを0に戻す
 		animation_count = 0;
 	}
+}
+
+void Bubble::MoveBubble(void)
+{
+	// 中心座標に角度と長さを使用した円の位置を加算する
+		// 度数法の角度を弧度法に変換
+	float radius = Angle * 3.14f / 180.0f;
+
+	// 三角関数を使用し、円の位置を割り出す。
+	float add_x = cos(radius) * Length;
+	float add_y = sin(radius) * Length;
+
+	// 結果ででた位置を中心位置に加算し、それを描画位置とする
+	location.x = CenterX + add_x;
+	//location.y = CenterY + add_y;
+
+	// 角度を変える
+	Angle += 2.0f;
+
+	//浮上する
+	CenterY -= 0.5f;
+	location.y = CenterY;
 }
--- bf2-4e/source/Bubble.h
+++ bf2-4e/source/Bubble.h
@@ -10,6 +10,15 @@ private:
 
 	int now_image;							// 今から表示される画像
 	int next_image;							// 次に表示される画像
+
+	float PosX;		// 描画座標X
+	float PosY;		// 描画座標Y
+	float Radius;		// 半径(描画用)
+	float CenterX;	// 中心座標X
+	float CenterY;	// 中心座標Y
+	float Angle;		// 角度
+	float Length;		// 半径の長さ
+
 public:
 	//コンストラクタ
 	Bubble();
@@ -25,5 +34,8 @@ public:
 
 	//シャボン玉のアニメーション関数
 	void BubbleAnimation(void);
+
+	//シャボン玉の移動
+	void MoveBubble(void);
 
 };
